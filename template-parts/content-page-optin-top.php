7bd8cf7dd3cb1dd027611f926bf2250761dc0ad2
ido barnea <barbareshet@gmail.com>
2017-01-18 23:13:27 +0200

    style & btn's

--- template-parts/content-page-optin-top.php
+++ template-parts/content-page-optin-top.php
@@ -19,13 +19,13 @@
                 </p>
             </div>
         </div>
-        <div class="row about-optin-btn-wrap">
-            <div class="col-sm-6 col-xs-6 quote-btn-'רשפ text-right">
+        <div class="row top-optin-btn-wrap">
+            <div class="col-sm-6 col-xs-6 quote-btn-wrap text-right">
                 <a href="<?php the_field('optin_btn_left_link');?>" class="btn quote-btn btn-info">
                     <?php the_field('optin_btn_left_text');?>
                 </a>
             </div>
-            <div class="col-sm-6 col-xs-6 book-btn-'רשפ text-left">
+            <div class="col-sm-6 col-xs-6 book-btn-wrap text-left">
                 <a href="<?php the_field('optin_btn_right_link');?>" class="btn book-btn btn-info">
                     <?php the_field('optin_btn_right_text');?>
                 </a>
