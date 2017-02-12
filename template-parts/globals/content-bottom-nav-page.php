cb1a6de45c0044007f71daad94003b49f1989678
ido barnea <barbareshet@gmail.com>
2017-02-13 00:06:19 +0200

    bottom nav
    added social links fields

--- template-parts/globals/content-bottom-nav-page.php
+++ template-parts/globals/content-bottom-nav-page.php
@@ -3,9 +3,10 @@
         <div class="container">
             <div class="col-sm-3 col-xs-12 social-links">
                 <ul class="nav navbar-nav">
-                    <li><a href="#"><i class="fa fa-facebook"></i> </a></li>
-                    <li><a href="#about"><i class="fa fa-google-plus"></i> </a></li>
-                    <li><a href="#contact"><i class="fa fa-yelp"></i> </a></li>
+                    <li><a href="<?php the_field('company_fb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i> </a></li>
+                    <li><a href="<?php the_field('company_twitter','options');?>" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i> </a></li>
+                    <li><a href="<?php the_field('company_gmb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i> </a></li>
+                    <li><a href="<?php the_field('company_yelp','options');?>" target="_blank" rel="nofollow"><i class="fa fa-yelp"></i> </a></li>
                 </ul>
             </div>
             <div id="navbar" class="col-sm-9 col-xs-12 footer-nav">
