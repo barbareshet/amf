11cf89790406157bf22f32baa9bb8b5a57ca43f0
ido barnea <barbareshet@gmail.com>
2017-02-15 09:01:21 +0200

    footer and schema changes

--- template-parts/globals/content-bottom-nav-page.php
+++ template-parts/globals/content-bottom-nav-page.php
@@ -2,12 +2,13 @@
     <nav class="navbar navbar-default" id="bottom-navigation">
         <div class="container">
             <div class="col-sm-3 col-xs-12 social-links">
-                <ul class="nav navbar-nav">
+                <ul class="list-inline">
                     <li><a href="<?php the_field('company_fb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i> </a></li>
                     <li><a href="<?php the_field('company_twitter','options');?>" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i> </a></li>
                     <li><a href="<?php the_field('company_gmb','options');?>" target="_blank" rel="nofollow"><i class="fa fa-google-plus"></i> </a></li>
                     <li><a href="<?php the_field('company_yelp','options');?>" target="_blank" rel="nofollow"><i class="fa fa-yelp"></i> </a></li>
                 </ul>
+                <?php get_template_part( 'template-parts/globals/content', 'schema' );?>
             </div>
             <div id="navbar" class="col-sm-9 col-xs-12 footer-nav">
                 <?php wp_nav_menu( array(
